829d7167c32b46d7425244730fed0c05aeb075ec
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-02-09 20:11:57 -0600

    Agregamos la ruta del enpoint de prueba y cambiamos el nombre de la otra

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,10 @@ const userController = require('../controllers/user');
 const router = express.Router();
 
 // Endpoint para agregar un usuario
-router.post('/users', userController.createUser);
+
+// GET
+router.get('/prueba', userController.prueba);
+// POST
+router.post('/create', userController.createUser);
 
 module.exports = router;
