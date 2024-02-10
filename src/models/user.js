917c08fb1016d67ff2b945f4d30b5d8485d63452
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-02-09 19:43:29 -0600

    Agregamos el modelo de usuario

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,34 @@
-const mongoose =  require('mongoose');
-// Usaremos los esquemas
-const Schema = mongoose.Schema;
+const { Schema, model } = require('mongoose');
 
 // Creamos el objeto del esquema y sus atributos
 const UserSchema = Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    imagen: {
+        type: String,
+        default: 'defaulf.png',
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
 });
 
 // Exportamos el modelo para usarlo en otros ficheros
