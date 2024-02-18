c69a8f44144f94df1a4e521fde57d5ca8346a879
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-02-17 22:07:49 -0600

    Creamos el token e instalamos la librería para darle expiracion

---
+++ src/services/jwt.js
@@ -0,0 +1,21 @@
+const jwt = require('jwt-simple');
+const moment = require('moment');
+require('dotenv').config({ path: '.env' });
+
+// Clave secreta
+const secret = process.env.SECRET;
+
+exports.createToken = (user) => {
+    const payload = {
+        id: user._id,
+        name: user.name,
+        username: user.username,
+        age: user.age,
+        email: user.email,
+        image: user.imagen,
+        iat: moment().unix(),
+        exp: moment().add(30, "days").unix(),
+    }
+
+    return jwt.encode(payload, secret);
+}
