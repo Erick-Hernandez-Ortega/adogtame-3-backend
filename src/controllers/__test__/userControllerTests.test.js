a08ecc39f5307018e7e86e2b074d8c7a89538d1e
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-06-01 13:31:01 -0600

    Agregando test para crear usuario

--- src/controllers/__test__/userControllerTests.test.js
+++ src/controllers/__test__/userControllerTests.test.js
@@ -5,8 +5,9 @@ const app = require("../../index");
 describe("POST /user", () => {
     it('Should display a 400 error for invalid input', async () => {
         const response = await request(app).post('/user').send({
-
         });
-        console.log(response.body);
+        
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('error')
     })
 })
--- src/validations/userValidations.js
+++ src/validations/userValidations.js
@@ -6,32 +6,32 @@ const validateUserCreation = async (req, res, next) => {
     const { name, age, email, password, username } = req.body;
 
     if (!name || !age || !email || !password || !username) {
-        return res.status(400).json({ status: "Error", message: "Falta un campo para la creación de usuario" });
+        return res.status(400).json({ status: "Error", error: "Falta un campo para la creación de usuario" });
     }
     if (!isValidEmail(email)) {
-        return res.status(400).json({ status: "Error", message: "Error de formato de email" });
+        return res.status(400).json({ status: "Error", error: "Error de formato de email" });
     }
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-        return res.status(409).json({ status: "Error", message: "El email proporcionado ya está en uso" });
+        return res.status(409).json({ status: "Error", error: "El email proporcionado ya está en uso" });
     }
 
     const existingUsername = await User.findOne({ username });
     if (existingUsername) {
-        return res.status(409).json({ status: "Error", message: "El username proporcionado ya está en uso" });
+        return res.status(409).json({ status: "Error", error: "El username proporcionado ya está en uso" });
     }
 
     if (isNaN(age)) {
-        return res.status(400).json({ status: "Error", message: "Error edad debe ser un numero" });
+        return res.status(400).json({ status: "Error", error: "Error edad debe ser un numero" });
     }
 
     if (!isValidName(name)) {
-        return res.status(400).json({ status: "Error", message: "Error nombre NO debe contener numeros" });
+        return res.status(400).json({ status: "Error", error: "Error nombre NO debe contener numeros" });
     }
 
     if (!isValidPassword(password)) {
-        return res.status(400).json({ status: "Error", message: "Error la contraseña debera de tener una mayuscula un numero y" });
+        return res.status(400).json({ status: "Error", error: "Error la contraseña debera de tener una mayuscula un numero y" });
     }
 
     next();
@@ -58,17 +58,17 @@ const validateLogin = async (email, pwd) => {
             $or: [{ email: email }, { username: email }]
         });
         if (!existingUser) {
-            return { status: "error", message: "El correo no coincide con ningún usuario" };
+            return { status: "error", error: "El correo no coincide con ningún usuario" };
         }
         const existingPwd = await bcrypt.compare(pwd, existingUser.password);
         if (!existingPwd) {
-            return { status: "error", message: "La contraseña no coincide" };
+            return { status: "error", error: "La contraseña no coincide" };
         }
         await User.updateOne({ _id: existingUser._id }, { isTokenRemoved: false });
         return { status: 'success', user: existingUser, }
     } catch (error) {
         console.error('Error al validar el inicio de sesión:', error);
-        return res.status(500).json({ message: "Internal error" });
+        return res.status(500).json({ error: "Internal error" });
     }
 };
 
